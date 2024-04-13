490f23a56d376a2217d48a7da7ad49452d2b9dcb
Jolieboo <duyb2111789@student.ctu.edu.vn>
2024-04-13 17:05:02 +0700

    TD push

--- app/views/page/Home_page.php
+++ app/views/page/Home_page.php
@@ -1,3 +1,12 @@
+<style>
+    .part2 {
+        background-color: #FF737E;
+        /* display:flex; */
+        /* justify-content: center; */
+        align-items: center;
+        color: white;
+    }
+</style>
 <div>
     <div id="carouselExampleIndicators" class="carousel slide my-1" data-bs-ride="true">
         <div class="carousel-indicators">
@@ -26,38 +35,27 @@
         </button>
     </div>
 </div>
-<div class="container">
-    <div class="row justify-content-around my-5">
-        <div class="col-md-5 justify-content-center d-flex">
-            <img src="/img/intro-home/silk.jpg" alt="" width="100%" class="rounded-5">
-        </div>
-        <div class="col-md-7 d-flex justify-content-center align-items-center">
-            <div>
-                <h4>QUAN TRỌNG CỦA VIỆC CHỌN VẢI TỐT TRONG QUÁ TRÌNH MAY ĐỒ</h4>
-                <p style="text-align: justify">Trong quá trình chọn vải cho việc may đồ, việc lựa chọn loại vải phù hợp là quan trọng. Tùy thuộc vào mục đích sử dụng và phong cách thiết kế, việc chọn vải tốt đòi hỏi sự tỉ mỉ và am hiểu về các loại vải khác nhau để đảm bảo sự thoải mái, đẹp mắt và độ bền của sản phẩm cuối cùng.</p>
-            </div>
-        </div>
+<div class="container-fluid part2 ">
+    <div class="row text-center pb-2">
+        <h1>CHÚNG MÌNH CHUYÊN VỀ</h1>
     </div>
-    <div class="row justify-content-around my-7">
-        <div class="col-md-7 d-flex justify-content-center align-items-center">
-            <div>
-                <h4>BÍ QUYẾT CHO QUẦN ÁO LUÔN SẠCH SẼ VÀ VÀO NẾP</h4>
-                <p style="text-align: justify">Quy trình giặt ủi chuyên nghiệp không chỉ đảm bảo sạch sẽ mà còn bảo vệ chất lượng và màu sắc của quần áo. Từ việc phân loại, sử dụng sản phẩm chăm sóc đúng cách đến kỹ thuật ủi tinh tế, mỗi bước đều được thực hiện với tinh thần cẩn trọng và kỹ lưỡng, tạo nên sự hoàn hảo và sự tự tin khi mặc.
-                </p>
-            </div>
+    <div class="row d-flex justify-content-center align-items-center">
+        <div class="col-md-3 d-flex justify-content-center align-items-center ">
+            <a href=""><img src="/img/logo/Aodai.svg" alt=""></a>
+            <h2>Áo dài</h2>
         </div>
-        <div class="col-md-5 justify-content-center d-flex">
-            <img src=" /img/intro-home/iron.jpg" alt="" width="100%" class="rounded-5">
+        <div class="col-md-3 d-flex justify-content-center align-items-center">
+            <a href=""><img src="/img/logo/Dress.svg" alt=""></a>
+            <h2>Đầm dạ hội</h2>
         </div>
-    </div>
-    <div class="row justify-content-around mt-7 pb-2">
-        <div class="col-md-5 justify-content-center d-flex ">
-            <img src="/img/intro-home/save.jpg" alt="" width="100%" class="rounded-5">
+        <div class="col-md-3 d-flex justify-content-center align-items-center">
+            <a href=""><img src="/img/logo/Vest.svg" alt=""></a>
+            <h2>Vest</h2>
         </div>
-        <div class="col-md-7 d-flex justify-content-center align-items-center">
-            <div>
-                <h4>LỢI ÍCH CỦA THUÊ TRANG PHỤC NHÀ DYLA</h4>
-                <p style="text-align: justify">Việc thuê trang phục có sẵn mang lại lợi ích về tiết kiệm chi phí, đa dạng lựa chọn phong cách, thuận tiện trong việc chuẩn bị cho các sự kiện và giảm bớt áp lực về việc bảo quản trang phục sau sự kiện. Đồng thời, nó còn giúp tạo ra sự mới mẻ trong phong cách mỗi khi cần và góp phần giảm lượng rác thải từ việc mua sắm trang phục mới.</div>
+        <div class="col-md-3 d-flex justify-content-center align-items-center">
+            <a href=""><img src="/img/logo/Cosplay.svg" alt=""></a>
+            <h2>Cosplay</h2>
         </div>
+
     </div>
 </div>
